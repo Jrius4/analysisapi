7ccf2fbcbf6df63991f25ec582b4ae595f3a2feb
Kazibwe Julius Jr <kazibwejuliusjunior@gmail.com>
2021-03-17 21:12:26 +0300

    real fast api code

--- main.py
+++ main.py
@@ -1,39 +1,7 @@
-import os
-import shutil
-from fastapi import FastAPI,File,UploadFile
-from fastapi.responses import FileResponse
-from typing import List
+from fastapi import FastAPI
 
 app = FastAPI()
-cwd = os.getcwd()
-
 
 @app.get('/')
 def index():
-    return {'data':{'name':'kazibwe'}}
-
-@app.get('/about')
-def about():
-    return {'data':'about page'}
-
-@app.get("/farmer",responses={200:{'description':'picture of a man','content':{'image/jpeg':{'example':'No examplae. Just imagine a picture of a farmer.'}}}})
-def farmer():
-    img_path = 'assets/imgs/man.JPG'
-    file_path = os.path.join(cwd,img_path)
-    if os.path.exists(file_path):
-        return FileResponse(file_path,media_type='image/jpeg',filename='farmer.jpg')
-        # return FileResponse(file_path)
-    return {'error':'file not found!'}
-
-@app.post('/post_farmer/')
-def post_farmer(
-    name:str,files:List[UploadFile] = File(...)
-            ):
-            url = []
-            for file in files:
-                with open('assets/uploads/'+file.filename,'wb') as buffer:
-                    shutil.copyfileobj(file.file,buffer)
-                url.append(file)
-
-
-            return {'name':name,'file':file,'url':url}
+    return {'data':{'name':'Kazibwe Julius Jr'}}
--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 source ./env/Scripts/activate
 pip freeze > requirements.txt
 uvicorn main:app --reload
+git add . && git commit -m "%%%phraze 2%%%"
