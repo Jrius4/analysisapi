30238a36a3c8cd2c8c801a338cdb5146903a2915
Kazibwe Julius Jr <kazibwejuliusjunior@gmail.com>
2021-03-19 04:54:55 +0300

    post tackled

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Kazibwe Julius Jr'}}
+@app.get('/blog')
+def index(limit:int = 10,
+        published:bool = True,
+        sort:Optional[str]=None):
+    if published:
+        return {'data':{f"{limit} published blogs, which is {published}"}}
+    else:
+        return {'data':{f"{limit} blogs, which is {published}"}}
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]=False
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {'data':f"blog is created title as {blog.title}"}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id : int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit:int=5):
+    return {'data':{id,limit}}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app,host="127.0.0.1",port=9000)
--- start.bat
+++ start.bat
@@ -2,3 +2,4 @@ source ./env/Scripts/activate
 pip freeze > requirements.txt
 uvicorn main:app --reload
 git add . && git commit -m "%%%phraze 2%%%"
+git push -u origin master
